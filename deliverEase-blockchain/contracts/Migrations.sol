0134a68314816870784a1ce20a0e2fa46a68711d
Nishal <nishaldevadiga2003@gmail.com>
2025-02-18 00:51:48 +0530

    commit

---
+++ deliverEase-blockchain/contracts/Migrations.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+    address public owner;
+    uint public last_completed_migration;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function setCompleted(uint completed) public {
+        require(
+            msg.sender == owner,
+            "Only the owner can set the completed migration"
+        );
+        last_completed_migration = completed;
+    }
+}
